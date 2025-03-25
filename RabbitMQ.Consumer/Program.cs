85b7631f025a2cb6a89c426e25af078e340f493a
KADIRCAN KELEBEK <kelebekkadircan@gmail.com>
2025-03-25 13:50:04 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿// CONSUMER
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Consumer is running...");
+
+
+ConnectionFactory factory = new();
+
+// bağlantı oluşturma
+factory.Uri = new("amqps://fkudeoip:aU7HKGiz5pqCZ1YiEDj_PKeJK0dvCKAs@rattlesnake.rmq.cloudamqp.com/fkudeoip");
+
+// bağlantıyı aktifleştirme ve kanal açma
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+// queue oluşturma
+await channel.QueueDeclareAsync(queue: "example-queue",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+// publisher ile aynı yapılandırmada olmalıdır.
+
+// queue dinleme
+AsyncEventingBasicConsumer eventConsumer = new(channel);
+ await channel.BasicConsumeAsync(queue: "example-queue", autoAck: false, consumer: eventConsumer);
+
+ eventConsumer.ReceivedAsync += async (object sender, BasicDeliverEventArgs @event) =>
+{
+    string message = Encoding.UTF8.GetString(@event.Body.ToArray());
+    Console.WriteLine($"Gelen Mesaj: {message}");
+    // mesaj işlendiğinde rabbitmq'ya mesajın işlendiğine dair bilgi verilir.
+    await channel.BasicAckAsync(@event.DeliveryTag, false);
+    // mesaj işlenmediğinde rabbitmq'ya mesajın işlenmediğine dair bilgi verilir.
+    //await channel.BasicNackAsync(@event.DeliveryTag,multiple: false,requeue: false);
+
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,39 @@
+﻿// Publisher
+using RabbitMQ.Client;
+using System.IO.Pipelines;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+// bağlantı oluşturma
+factory.Uri = new("amqps://fkudeoip:aU7HKGiz5pqCZ1YiEDj_PKeJK0dvCKAs@rattlesnake.rmq.cloudamqp.com/fkudeoip");
+
+// bağlantıyı aktifleştirme ve kanal açma
+using IConnection connection = await  factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+
+// queue oluşturma
+await channel.QueueDeclareAsync(queue: "example-queue",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+
+
+// queue mesaajı gönderme
+// rabbitmq mesajları byte türünden kabul eder . mesajları byte dönüştürmemiz gerekir.
+
+var props = new BasicProperties();
+props.Persistent = true;
+
+
+for (int i = 0; i < 20; i++)
+{
+byte[] mesaj = Encoding.UTF8.GetBytes("Hello World! " + i);
+    await channel.BasicPublishAsync(exchange: "", routingKey: "example-queue",mandatory:true,basicProperties: props , body: mesaj);
+   await Task.Delay(300);
+}
+
+Console.Read();
